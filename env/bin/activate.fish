e10414610d6785c8565d7e6eba73b618aa594f2b
Aiza <aizaderisyana@yahoo.co.id>
2023-09-10 17:15:48 +0700

    commit zumart

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/aizaderisyana/PBP/ZUMART/env"
+set -gx VIRTUAL_ENV "/Users/aizaderisyana/PBP/ZUMART real/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- main/views.py
+++ main/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
 def show_main(request):
     context = {
-        'name': ['Nike','Adidas', ],
-        'amount': ['6', '4', '3'],
-        'description': ['Air Jordan', 'Samba', '550'],
-        'size': ['38', '39', '40'],
-        'price': [2000000, 1200000, 1500000], 
+        'NAJ': {
+            'Description': 'bagus',
+            'Size': 38,
+            'Amount': 12,
+            'Price': 2300000, 
+        },
+        'AS': {
+            'Description': 'cool',
+            'Size': 40,
+            'Amount': 14,
+            'Price': 2500000, 
+        },
+        'NB550': {
+            'Description': 'ini dia',
+            'Size': 37,
+            'Amount': 7,
+            'Price': 1500000, 
+        },
+        'NDL': {
+            'Description': 'edgy',
+            'Size': 42,
+            'Amount': 3,
+            'Price': 2600000, 
+        },
+        'CCT': {
+            'Description': 'classic',
+            'Size': 36,
+            'Amount': 17,
+            'Price': 1000000, 
+        },
     }
-
-    return render(request, "main.html", {'context': context})
+    return render(request, "main.html", context)
