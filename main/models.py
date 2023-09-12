fbcf60ec66232b32c781758720d207fbfcffe016
Aiza <aizaderisyana@yahoo.co.id>
2023-09-12 16:51:22 +0700

    ZUMART CORRECTED

--- main/models.py
+++ main/models.py
@@ -2,8 +2,8 @@ from django.db import models
  
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, default="default_value")
-    kelas = models.CharField(max_length=255,  default="default_value")
+    name = models.CharField(max_length=255)
+    kelas = models.CharField(max_length=255)
     description = models.TextField()
-    appName = models.CharField(max_length=255,  default="default_value")
+    appName = models.CharField(max_length=255)
     amount = models.IntegerField()
